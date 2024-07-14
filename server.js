c8ed3b4c1ca099ac0ee551ef8dc2910a06a69ce7
OskariSuonpaa <oskari.suonpaa@student.lut.fi>
2024-07-14 09:50:58 +0300

    chore: Add endpoint for fetching starred repositories

--- server.js
+++ server.js
@@ -11,9 +11,26 @@ const BASE_URL = 'https://api.github.com';
 app.use(express.json());
 app.use(cors());
 
+
 app.get('/repos/:username', async (request, response) => {
   const { username } = request.params;
   const url = `${BASE_URL}/users/${username}/repos`;
+
+  try {
+    const repos = await fetch(url, {
+      headers: { Authorization: TOKEN ? `token ${TOKEN}` : '' },
+    });
+    const data = await repos.json();
+    const parsedData = await dataParser.parseData(data, TOKEN);
+    response.json(parsedData);
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/repos/:username/starred', async (request, response) => {
+  const { username } = request.params;
+  const url = `${BASE_URL}/users/${username}/starred`;
 
   try {
     const repos = await fetch(url, {
